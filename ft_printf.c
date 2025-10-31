dbd1e1baaa4fd556bbd52e046efc744356fde69c
lenasticot <lena.odum1@gmail.com>
2025-10-31 17:52:59 +0100

    % / str / char / int / dig checked

--- ft_printf.c
+++ ft_printf.c
@@ -1,10 +1,55 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 char special_string(char c)
 {
 	return (0);
+}
+
+int	count_nbr(int a)
+{
+	int	i;
+
+	i = 0;
+	if (a <= 0)
+		i++;
+	while (a != 0)
+	{
+		i++;
+		a = a / 10;
+	}
+	return (i);
+}
+
+char	*ft_itoa(int n)
+{
+	char		*result;
+	int			token;
+	int			is_neg;
+	long int	num;
+
+	num = n;
+	is_neg = (num < 0);
+	token = count_nbr(num);
+	result = malloc(sizeof(char) * (token + 1));
+	if (result == NULL)
+		return (NULL);
+	result[token--] = '\0';
+	if (is_neg)
+	{
+		num = -num;
+		result[0] = '-';
+	}
+	if (num == 0)
+		result[0] = '0';
+	while (num > 0)
+	{
+		result[token--] = (num % 10) + '0';
+		num = num / 10;
+	}
+	return (result);
 }
 
 int	whatisthis(char s, char spechar)
@@ -14,23 +59,8 @@ int	whatisthis(char s, char spechar)
 
 int ft_printf(const char *c, ...)
 {
-
-    // while char c
-    // if c == %
-    // check for next character
-    // act upon and print with special rule
-    // if special character (eg "\")
-    // check for next character and act upon
-    // else
-    // print c with write
-
-// int i;
-
 va_list special;
 va_start(special, c);
-
-// this is the basic print situation without any problem
-// do i need to count how many arguments I will receive? 
 
 int token;
 int i;
@@ -42,9 +72,13 @@ j = 0;
 token = 0;
 while (c[i])
 {
-    if (whatisthis(c[i], '%'))
+    while (whatisthis(c[i], '%'))
     {
         i++;
+		// check for %
+		if(whatisthis(c[i], '%'))
+			break;
+		//check for char//string
         if (whatisthis(c[i], 's') || (whatisthis(c[i], 'c')))
         { 
 			result = va_arg(special, char*);
@@ -57,9 +91,21 @@ while (c[i])
 				token++;
 			}
         }
-		if (whatisthis(c[i], 'd'))
+		// check for int/digit
+		if ((whatisthis(c[i], 'd')) || (whatisthis(c[i], 'i')))
+		{
 
-         // need to count the characters from the special charaacters as well
+		
+			result = ft_itoa(va_arg(special, int));
+			i++;
+			j = 0;
+			while (result[j])
+			{
+				write(1, &result[j], 1);
+				j++;
+				token++;
+			}
+		}
     }
     write(1, &c[i], 1);
     i++;
@@ -71,9 +117,19 @@ return (token);
 
 int main (void)
 {
-char *c = "He %s llo %s worl %s d";
+char *c = "He %i%s%% llo %s world. What about %% ?\n Now let's think about\n";
 int result;
+int ouiresult;
 
-result = ft_printf(c, "123", "ta darone", "ntm");
-printf("%i", result);
+result = ft_printf(c, 123, "ta darone", "ntm", "hmm");
+printf("%i\n", result);
+
+ouiresult = printf(c, 123, "ta darone", "ntm", "hmm");
+printf("%i\n", result);
+
+
 }
+
+			// need to check if va_arg isanum
+			// if not its not working
+			// actually its ok normal reaction of the function
